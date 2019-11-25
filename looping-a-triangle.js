2396b714ddf009c2173bf40c4baf2e8ec130bf92
Aaron Devine <a.f.devine@gmail.com>
2019-11-24 19:26:09 -0800

    updated commit with commented instructions

--- looping-a-triangle.js
+++ looping-a-triangle.js
@@ -1,3 +1,12 @@
+//Write a loop that makes seven calls to console.log to output the following triangle:
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
 function printTriangle () {
   val = "#";
   for (let x=0; x<7; x++) {
