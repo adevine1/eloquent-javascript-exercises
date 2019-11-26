eddc5908eb87a306ddc83bd74a85fab9d6cf00c4
Aaron Devine <51430324+adevine1@users.noreply.github.com>
2019-11-25 16:29:44 -0800

    Update minimum.js

--- minimum.js
+++ minimum.js
@@ -1,5 +1,5 @@
 /*
-Chapter 2 introuced the standard function Math.min, which returns its smallest argument (see 'Return Values' on page 27).
+Chapter 2 introduced the standard function Math.min, which returns its smallest argument (see 'Return Values' on page 27).
 We can build something like that now. Write a function *min* that takes two arguments and returns their minimum.
 */
 
